96af32a6f2cd94d08ebdeb580372df9634487f4f
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2024-03-09 14:12:05 -0600

    update funcs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+/** Gets users timezone from system settings */
 export function getUsersTimeZone(): TimeZone {
   return Intl.DateTimeFormat().resolvedOptions().timeZone as TimeZone;
 }
-
+/** Returns a Date object set to custom timezone */
 export function setUserTimeZone(timeZone: TimeZone): string {
   const setzone = new Date().toLocaleString('en-US', { timeZone });
   return setzone;
